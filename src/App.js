352acd2b71acc6edba95cda66ace84450d6ad963
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2020-12-31 14:36:18 +0200

    added shop page and added collections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Switch , Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.componet';
+
+
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>    
+        <Route exact path='/shop' component={ShopPage}/>     
+      </Switch>
     </div>
   );
 }
