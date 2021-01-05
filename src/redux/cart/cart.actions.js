705c01e67d27614b2046dde21a78bf69a779cdc4
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-01-05 09:18:30 +0200

    Added cart Item util and updated shop data

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
-import { CartActionTypes } from './cart.types';
+import { CartActionTypes } from "./cart.types";
 
-export const toggelCartHidden = () =>( {
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+export const toggelCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
