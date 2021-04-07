60a4ea98a913cb7cab3a3a771ad2c485124b3428
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-04-07 08:50:36 +0200

    change working files

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,18 +15,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.post("/payment", (req, res) => {
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: "usd",
+    currency: 'usd'
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
@@ -38,9 +42,4 @@ app.post("/payment", (req, res) => {
       res.status(200).send({ success: stripeRes });
     }
   });
-});
-
-app.listen(port, (error) => {
-  if (error) throw error;
-  console.log("Server running on port " + port);
 });
